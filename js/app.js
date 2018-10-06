dde995fa0332a13ee6f7bd27ff137e9455efc3ef
Jal Panchal <jal08panchal@gmail.com>
2018-10-05 22:38:19 -0700

    Creating object for Player constrcutor

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ class Player{
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
